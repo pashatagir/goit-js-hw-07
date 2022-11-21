58d4e5ef89b039ec1d224ef2db8115525fa52b55
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-21 10:51:34 +0200

    fixed task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainerEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGalleryMarkup = createCardsImageMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+galleryContainerEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="1280" height="855">`,
+    {
+      onShow: (instance) => {
+        galleryContainerEl.addEventListener("keydown", onGalleryClickEsc);
+      },
+      onClose: (instance) => {
+        galleryContainerEl.removeEventListener("keydown", onGalleryClickEsc);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onGalleryClickEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+function createCardsImageMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+    </a>
+    </div>
+    `;
+    })
+    .join("");
+}
